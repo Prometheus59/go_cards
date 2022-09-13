fd33abae5f0dc9ab0762825123cb4f2b04134031
Ryan Karumanchery <ryankarumanchery@gmail.com>
2022-09-12 20:14:30 -0400

    Added deck functions to save and retrieve deck to/from file

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 func main() {
-	cards := newDeck()
 
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
 	cards.print()
+
 }
 
 func newCard() string {
